99196c79d3f62d0e9486f19a3d89e8ce95797c05
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-11-11 22:52:44 -0800

    adding initial structure for decision boundary and instruction button

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -13,6 +13,8 @@ export default (props)=> {
     selectedData,
     handleDataClick,
     dataRevision,
+    decisionInt,
+    decisionCoef,
   } = props;
 
   const createLayout = () => {
@@ -68,6 +70,61 @@ export default (props)=> {
       },
     }
     dataTrace.push(selectedTrace);
+
+    const a = -1 * decisionCoef[0][2] / decisionCoef[0][3];
+    const intercept = decisionInt[0] / decisionCoef[0][3];
+    const a1 = -1 * decisionCoef[1][2] / decisionCoef[1][3];
+    const intercept1 = decisionInt[1] / decisionCoef[1][3];
+    const a2 = -1 * decisionCoef[2][2] / decisionCoef[2][3];
+    const intercept2 = decisionInt[2] / decisionCoef[2][3];
+
+    const y_a1 = -1 * ((decisionCoef[0][0] * 1) / decisionCoef[0][1] + (decisionInt[0] / decisionCoef[0][1]))
+    const y_a2 = -1 * ((decisionCoef[0][0] * 5) / decisionCoef[0][1] + (decisionInt[0] / decisionCoef[0][1]))
+
+    const y_b1 = -1 * ((decisionCoef[1][0] * 1) / decisionCoef[1][1] + (decisionInt[1] / decisionCoef[1][1]))
+    const y_b2 = -1 * ((decisionCoef[1][0] * 5) / decisionCoef[1][1] + (decisionInt[1] / decisionCoef[1][1]))
+
+    const y_c1 = -1 * ((decisionCoef[2][0] * 1) / decisionCoef[2][1] + (decisionInt[2] / decisionCoef[2][1]))
+    const y_c2 = -1 * ((decisionCoef[2][0] * 5) / decisionCoef[2][1] + (decisionInt[2] / decisionCoef[2][1]))
+
+    const decisionTrace1 = {
+      x: [1, 5],
+      y: [y_a1, y_a2],
+      mode: 'lines',
+      types: 'scatter',
+      name: 'Decision',
+      marker: {
+        color: 'pink',
+        size: 12,
+      },
+    }
+    dataTrace.push(decisionTrace1);
+
+    const decisionTrace2 = {
+      x: [1, 5],
+      y: [y_b1, y_b2],
+      mode: 'lines',
+      types: 'scatter',
+      name: 'Decision',
+      marker: {
+        color: 'blue',
+        size: 12,
+      },
+    }
+    dataTrace.push(decisionTrace2);
+
+    const decisionTrace3 = {
+      x: [1, 5],
+      y: [y_c1, y_c2],
+      mode: 'lines',
+      types: 'scatter',
+      name: 'Decision',
+      marker: {
+        color: 'green',
+        size: 12,
+      },
+    }
+    dataTrace.push(decisionTrace3);
 
     return dataTrace;
   }
